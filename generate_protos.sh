cc8340cb204e78ae73b19c1563cbc01198d3598d
Luiz Pereira <luizfper@gmail.com>
2023-11-07 16:41:25 -0500

    remove V1 protos

--- generate_protos.sh
+++ generate_protos.sh
@@ -5,7 +5,6 @@ set -e
 rm -rf lib/google
 
 PROTOS="\
-  google/cloud/speech/v1/cloud_speech.proto \
   google/cloud/speech/v2/cloud_speech.proto \
   google/longrunning/operations.proto \
   google/rpc/status.proto \
