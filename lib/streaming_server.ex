f503926a08af0b488b6f9b07942c7941c6209526
Luiz Pereira <luiz@fireworkhq.com>
2023-11-10 19:46:01 -0500

    small modifications

--- lib/streaming_server.ex
+++ lib/streaming_server.ex
@@ -9,19 +9,20 @@ defmodule ExGoogleSTT.StreamingServer do
 
   The requests can be sent using `send_request/3`. Each request should be a
   `t:Google.Cloud.Speech.V2.StreamingRecognizeRequest.t/0` struct created using
-  `Google.Cloud.Speech.V2.StreamingRecognizeRequest.new/1` accepting keyword with struct fields.
+  `Google.Cloud.Speech.V2.StreamingRecognizeRequest`.
   This is an auto-generated module, so check out [this notice](readme.html#auto-generated-modules) and
   [API reference](https://cloud.google.com/speech-to-text/docs/reference/rpc/google.cloud.speech.V2#google.cloud.speech.V2.StreamingRecognizeRequest)
 
   ## Responses
 
   The client sends responses from API via messages to the target (by default it is the process that spawned client).
-  Each message is a struct `t:Google.Cloud.Speech.V2.StreamingRecognizeResponse.t/0` or a tuple with pid of sender and the same struct. Message format is controlled by `include_sender` option of a client.
+  Each message is a struct `t:Google.Cloud.Speech.V2.StreamingRecognizeResponse.t/0` or a tuple with pid of sender and the same struct.
+  Message format is controlled by `include_sender` option of a client.
 
   ## Usage
 
-  1. Start the client
-  1. Send request with `Google.Cloud.Speech.V2.StreamingRecognitionConfig`
+  1. Start the Server
+  1. Send a config request with `Google.Cloud.Speech.V2.StreamingRecognitionConfig`
   1. Send request(s) with `Google.Cloud.Speech.V2.RecognitionAudio` containing audio data
   1. (async) Receive messages conatining `Google.Cloud.Speech.V2.StreamingRecognizeResponse`
   1. Send final `Google.Cloud.Speech.V2.RecognitionAudio` with option `end_stream: true`
@@ -51,24 +52,9 @@ defmodule ExGoogleSTT.StreamingServer do
   - `monitor_target` - If set to true, a client will monitor the target and shutdown
   if the target is down
   - `include_sender` - If true, a client will include its pid in messages sent to the target.
-  - `start_time` - Time by which response times will be shifted in nanoseconds. Defaults to `0` ns.
   """
   @spec start_link(options :: Keyword.t()) :: {:ok, pid} | {:error, any()}
   def start_link(options \\ []) do
-    do_start(:start_link, options)
-  end
-
-  @doc """
-  Starts a client process without links.
-
-  See `start_link/1` for more info
-  """
-  @spec start(options :: Keyword.t()) :: {:ok, pid} | {:error, any()}
-  def start(options \\ []) do
-    do_start(:start, options)
-  end
-
-  defp do_start(fun, options) do
     options =
       options
       |> Map.new()
@@ -76,7 +62,7 @@ defmodule ExGoogleSTT.StreamingServer do
       |> Map.put_new(:monitor_target, false)
       |> Map.put_new(:include_sender, false)
 
-    apply(GenServer, fun, [__MODULE__, options])
+    GenServer.start_link(__MODULE__, options)
   end
 
   @doc """
@@ -84,6 +70,24 @@ defmodule ExGoogleSTT.StreamingServer do
   """
   @spec stop(client :: pid()) :: :ok
   defdelegate stop(pid), to: GenServer
+
+  @doc """
+  Sends a request to the API. If option `end_stream: true` is passed,
+  closes a client-side gRPC stream.
+  """
+  @spec send_config(client :: pid(), StreamingRecognizeRequest.t(), Keyword.t()) :: :ok
+  def send_config(_pid, _cfg_request, opts \\ [])
+
+  def send_config(
+        pid,
+        %StreamingRecognizeRequest{streaming_request: {:streaming_config, _}} = cfg_request,
+        opts
+      ) do
+    GenServer.cast(pid, {:send_requests, [cfg_request], opts})
+    :ok
+  end
+
+  def send_config(_pid, _cfg_request, _), do: raise("Bad Config Request")
 
   @doc """
   Sends a request to the API. If option `end_stream: true` is passed,
